256abe601c2344ad590b339f27c7f47fa94caa55
My Name <email@email.com>
2023-02-01 10:01:59 -0800

    Configured DBContext class to use SQL Server.

--- EntityFrameworkCoreExample/StudentContext.cs
+++ EntityFrameworkCoreExample/StudentContext.cs
@@ -7,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkCoreExample
 {
-    internal class StudentContext :DbContext
+    public class StudentContext : DbContext
     {
+        public StudentContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            //Server = The server we are connecting to. LocalDB is included with VS.
+            //Database = The desired name for the database.
+            //Trusted_Connection = Indicates that our windows account should be used.
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EFCoreExample;Trusted_Connection=True;");
+        }
     }
 }
